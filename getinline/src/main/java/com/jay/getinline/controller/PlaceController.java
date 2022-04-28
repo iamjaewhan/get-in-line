a935389851a3704d494703a6d6af30c347b0556c
jay <jaewhan232@naver.com>
2022-04-28 19:51:59 +0900

    Initial commit

---
+++ getinline/src/main/java/com/jay/getinline/controller/PlaceController.java
@@ -0,0 +1,9 @@
+package com.jay.getinline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/places")
+@Controller
+public class PlaceController {
+}
