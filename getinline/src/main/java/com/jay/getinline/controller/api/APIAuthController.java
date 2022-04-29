478d39c7404c9ca2db9d9a1a6eee2efe040b68cc
jay <jaewhan232@naver.com>
2022-04-29 12:52:10 +0900

    endpoint 선언

---
+++ getinline/src/main/java/com/jay/getinline/controller/api/APIAuthController.java
@@ -0,0 +1,22 @@
+package com.jay.getinline.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class APIAuthController {
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "done";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "done";
+    }
+
+}
