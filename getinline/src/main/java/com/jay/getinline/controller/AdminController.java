479fbaa7fbe856238b0168d01894b6d61f013a60
jay <jaewhan232@naver.com>
2022-04-29 14:14:09 +0900

    feat : admin/places 컨트롤러 구현

--- getinline/src/main/java/com/jay/getinline/controller/AdminController.java
+++ getinline/src/main/java/com/jay/getinline/controller/AdminController.java
@@ -1,17 +1,31 @@
 package com.jay.getinline.controller;
 
+import com.jay.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
     @GetMapping("/places")
-    public String getPlaces() {
-        return "";
+    public ModelAndView adminPlaces(
+            PlaceType placeType,
+            String placeName,
+            String address
+            ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeName);
+        map.put("placeName", placeName);
+        map.put("address", address);
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/{place-id}")
@@ -24,7 +38,7 @@ public class AdminController {
         return "";
     }
 
-    @GetMapping("/events/{event-id}")
+    @GetMapping("/events/{eventId}")
     public String getEvent(@PathVariable String eventId) {
         return "";
     }
