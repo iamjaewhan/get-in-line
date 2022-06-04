bd9d2c0507c2ff4495119b0561c6d06e59cdcd96
jay <jaewhan232@naver.com>
2022-06-04 16:46:26 +0900

    implement - PlaceControllerTest

--- getinline/src/main/java/com/jay/getinline/controller/PlaceController.java
+++ getinline/src/main/java/com/jay/getinline/controller/PlaceController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/places")
+@RequestMapping
 @Controller
 public class PlaceController {
 
-    @GetMapping("/")
+    @GetMapping("/places")
     public String places() {
         return "place/index";
     }
 
-    @GetMapping("/{placeId}")
-    public String placeDetail(@PathVariable Integer placeId) {
+    @GetMapping("/places/{placeId}")
+    public String placeDetail(@PathVariable Long placeId) {
         return "place/detail";
     }
 
