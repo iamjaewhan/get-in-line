dd31fcfe15b034887857020c3a92c66255a1c547
jay <jaewhan232@naver.com>
2022-06-04 15:42:58 +0900

    implement - APIAuthController , DTO 테스트

--- getinline/src/main/java/com/jay/getinline/controller/api/APIAuthController.java
+++ getinline/src/main/java/com/jay/getinline/controller/api/APIAuthController.java
@@ -1,22 +1,22 @@
 package com.jay.getinline.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.jay.getinline.DTO.APIDataResponse;
+import com.jay.getinline.DTO.AdminRequest;
+import com.jay.getinline.DTO.LoginRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
 public class APIAuthController {
 
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "done.";
+    @PostMapping("/sign-up")
+    public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest) {
+        return APIDataResponse.empty();
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "done.";
+    @PostMapping("/login")
+    public APIDataResponse<String> login(@RequestBody LoginRequest loginRequest) {
+        return APIDataResponse.empty();
     }
 
 }
