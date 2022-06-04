05bec9630ffee417ad7c11392bd239d3e837cb4b
jay <jaewhan232@naver.com>
2022-06-04 17:19:53 +0900

    refact : 파일명 변경(error2 -> error)

--- getinline/src/main/java/com/jay/getinline/controller/AuthController.java
+++ getinline/src/main/java/com/jay/getinline/controller/AuthController.java
@@ -2,12 +2,13 @@ package com.jay.getinline.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
 
     @GetMapping("/login")
-    public String login() {
+    public  String login() {
         return "auth/login";
     }
 
