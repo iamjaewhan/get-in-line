cf0549e83a86ae6ddaaca8026353b370d421337a
jay <jaewhan232@naver.com>
2022-06-07 17:19:01 +0900

    Revert "Implement - Validation error code"
    This reverts commit 7f31a0d0dc046f5c2c99ec775cc05910bfd46497.

--- getinline/src/main/java/com/jay/getinline/constant/ErrorCode.java
+++ getinline/src/main/java/com/jay/getinline/constant/ErrorCode.java
@@ -14,8 +14,6 @@ public enum ErrorCode {
 
     BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "bad request"),
     SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
-    VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation error"),
-
 
     INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "internal error"),
     SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error")
@@ -27,13 +25,13 @@ public enum ErrorCode {
 
 
     public String getMessage(Exception e) {
-        return getMessage(this.getMessage() + "-" + e.getMessage());
+        return getMessage(e.getMessage());
     }
 
     public String getMessage(String message) {
         return Optional.ofNullable(message)
                 .filter(Predicate.not(String::isBlank))
-                .orElse(this.getMessage());
+                .orElse(getMessage());
     }
 
     public boolean isClientSideError() {
--- getinline/src/main/java/com/jay/getinline/controller/error/APIExceptionHandler.java
+++ getinline/src/main/java/com/jay/getinline/controller/error/APIExceptionHandler.java
@@ -3,80 +3,44 @@ package com.jay.getinline.controller.error;
 import com.jay.getinline.DTO.APIErrorResponse;
 import com.jay.getinline.constant.ErrorCode;
 import com.jay.getinline.exception.GeneralException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-import javax.validation.ConstraintViolationException;
 import java.util.Map;
 
 @RestControllerAdvice(annotations = RestController.class)
-public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+public class APIExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<Object> general(ConstraintViolationException e, WebRequest request) {
-        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
-        );
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+    public ResponseEntity<APIErrorResponse> general(GeneralException e){
         ErrorCode errorCode = e.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ?
                 HttpStatus.BAD_REQUEST :
                 HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
-        );
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage()
+                ));
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+    public ResponseEntity<APIErrorResponse> general(Exception e){
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
-        );
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage()
+                ));
     }
 
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorCode errorCode = status.is4xxClientError() ?
-                ErrorCode.SPRING_BAD_REQUEST :
-                ErrorCode.SPRING_INTERNAL_ERROR;
 
-        return super.handleExceptionInternal(
-                ex,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
-                headers,
-                status,
-                request
-        );
-    }
 }
