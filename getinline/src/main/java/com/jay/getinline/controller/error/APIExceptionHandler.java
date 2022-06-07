9f2c366a5763ea46c367ca6470d27e4a993c5d67
jay <jaewhan232@naver.com>
2022-06-07 21:05:16 +0900

    Implement - ConstraintViolationException handler

--- getinline/src/main/java/com/jay/getinline/controller/error/APIExceptionHandler.java
+++ getinline/src/main/java/com/jay/getinline/controller/error/APIExceptionHandler.java
@@ -3,44 +3,81 @@ package com.jay.getinline.controller.error;
 import com.jay.getinline.DTO.APIErrorResponse;
 import com.jay.getinline.constant.ErrorCode;
 import com.jay.getinline.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.Map;
 
 @RestControllerAdvice(annotations = RestController.class)
-public class APIExceptionHandler {
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException e){
+    public ResponseEntity<Object> general(ConstraintViolationException e, WebRequest request) {
+        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
         ErrorCode errorCode = e.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ?
                 HttpStatus.BAD_REQUEST :
                 HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false, errorCode, errorCode.getMessage()
-                ));
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
     }
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(Exception e){
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false, errorCode, errorCode.getMessage()
-                ));
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorCode errorCode = status.is4xxClientError() ?
+                ErrorCode.SPRING_BAD_REQUEST :
+                ErrorCode.SPRING_INTERNAL_ERROR;
+
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                headers,
+                status,
+                request
+        );
+    }
 
 }
